bb91290c6b44827d372be3df0e58250284121086
KC <kcfike@gmail.com>
2020-04-03 16:43:20 -0600

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-appv1"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://yf0nl1azi0.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_NrcZs7ofr",
+    APP_CLIENT_ID: "6a8v8do5jgoen08kf04h74jhfr",
+    IDENTITY_POOL_ID: "us-east-1:c27e3280-8f87-4eb6-8f09-8acfeba1e681"
+  }
+};
